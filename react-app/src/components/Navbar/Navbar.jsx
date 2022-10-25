f20dbc4d1e70e04838211c734066ef83f286597c
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2022-10-25 05:25:59 -0300

    Update Navbar.jsx

--- react-app/src/components/Navbar/Navbar.jsx
+++ react-app/src/components/Navbar/Navbar.jsx
@@ -13,17 +13,19 @@ const Navbar = () => {
   return (
     <nav className='app__navbar'>
       <div className='app__navbar-logo'>
-        <img src={images.logo} alt="logo" />
+        <a href='#home'>
+          <img src={images.logo} alt='logo' />
+        </a>
       </div>
       <ul className='app__navbar-links'>
-        <li className='p__opensans'><a href="#home">Início</a></li>
-        <li className='p__opensans'><a href="#notifications">Notificações</a></li>
-        <li className='p__opensans'><a href="#projects">Projetos</a></li>
-        <li className='p__opensans'><a href="#rooms">Salas</a></li>
-        <li className='p__opensans'><a href="#settings">Configurações</a></li>
+        <li className='p__opensans'><a href='#home'>Início</a></li>
+        <li className='p__opensans'><a href='#notifications'>Notificações</a></li>
+        <li className='p__opensans'><a href='#projects'>Projetos</a></li>
+        <li className='p__opensans'><a href='#rooms'>Salas</a></li>
+        <li className='p__opensans'><a href='#settings'>Configurações</a></li>
       </ul>
       <div className='app__navbar-login'>
-        <a href="#login" className='p__opensans'>Entrar</a>
+        <a href='#login' className='p__opensans'>Entrar</a>
       </div>
       <div className='app__navbar-smallscreen'>
         <FaBars color='#fff' fontSize={27} onClick={() => setToggleMenu(true)} />
@@ -32,11 +34,11 @@ const Navbar = () => {
           <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
             <MdClose fontSize={27} className='overlay__close' onClick={() => setToggleMenu(false)} />
             <ul className='app__navbar-smallscreen_links'>
-              <li className='p__opensans'><a href="#home">Início</a></li>
-              <li className='p__opensans'><a href="#notifications">Notificações</a></li>
-              <li className='p__opensans'><a href="#projects">Projetos</a></li>
-              <li className='p__opensans'><a href="#rooms">Salas</a></li>
-              <li className='p__opensans'><a href="#settings">Configurações</a></li>
+              <li className='p__opensans'><a href='#home'>Início</a></li>
+              <li className='p__opensans'><a href='#notifications'>Notificações</a></li>
+              <li className='p__opensans'><a href='#projects'>Projetos</a></li>
+              <li className='p__opensans'><a href='#rooms'>Salas</a></li>
+              <li className='p__opensans'><a href='#settings'>Configurações</a></li>
             </ul>
           </div>
         )}
